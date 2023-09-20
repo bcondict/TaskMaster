33cd062b77496e55380d8870744bb3ebe89ed51a
Jesús <98342185+bcondict@users.noreply.github.com>
2023-09-20 18:16:16 +0000

    updating changes, comments and deleted useless coemnts

--- components/DeleteButton.js
+++ components/DeleteButton.js
@@ -1,32 +1,14 @@
 import styles from './DeleteButton.module.css'
 // import DeleteIcon from '../public/delete_button.svg'
+import DeleteIcon from '../public/delete_button_icon'
 
-const DeleteIcon = ({ fill }) => (
-  <svg viewBox="0 0 120 100" xmlns="http://www.w3.org/2000/svg">
-    <g id="xSymbol">
-      <g id="line">
-        <rect x="0.0561523" y="18.0294" width="25" height="120" rx="12.5" transform="rotate(-45 0.0561523 18.0294)"
-        // style={{fill: fill}}
-        style={styles.deleteButtonIcon}
-        />
-      </g>
-      <g id="line">
-        <rect x="84.9706" y="1.05603" width="25" height="120" rx="12.5" transform="rotate(45 84.9706 1.05603)"
-        // style={{fill: fill}}
-        style={styles.deleteButtonIcon}
-        />
-      </g>
-    </g>
-  </svg>
-)
 
 const DeleteButton = () => {
   return (
     <button
       className={styles.deleteButton}
     >
-      {/* <DeleteIcon fill="red" className={styles.deleteButtonIcon}/> */}
-      <DeleteIcon />
+      <DeleteIcon classGroup={styles.iconGroup} classChild={styles.iconChild }/>
     </button>
   )
 }
---
+++ public/delete_button_icon.js
@@ -0,0 +1,16 @@
+
+
+const DeleteIcon = ({ classGroup, classChild }) => (
+  <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+    <g className={classGroup} id="xSymbol">
+      <g className={classChild} id="line">
+        <rect x="0.0561523" y="18.0294" width="25" height="120" rx="12.5" transform="rotate(-45 0.0561523 18.0294)"/>
+      </g>
+      <g className={classChild} id="line">
+        <rect x="84.9706" y="1.05603" width="25" height="120" rx="12.5" transform="rotate(45 84.9706 1.05603)"/>
+      </g>
+    </g>
+  </svg>
+)
+
+export default DeleteIcon
