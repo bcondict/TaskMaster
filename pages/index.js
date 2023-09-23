a3807367055071a38a14226ee2f1d44c63802faf
Jesús <98342185+bcondict@users.noreply.github.com>
2023-09-23 20:51:07 +0000

    consuming API from backend

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import { Box, Container, Heading } from "@chakra-ui/react";
 import AddTaskButton from "../components/AddTaskButton";
 import CustomCheckbox from "../components/CustomCheckbox"
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5555/tasks")
+      .then((res) => {
+        setTasks(res.data.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, []);
+
   return (
     <Container maxW="container" >
       <Box
         borderRadius={"lg"}
         bg={"palidBlue"}
-        // bg={"#538499"}
-        // bg={"#6EB0CC"}
         p={3}
-        // h={100}
         minH={"90vh"}
       >
         <Heading
@@ -23,9 +36,13 @@ export default function Home() {
         >
           Today's Task
         </Heading>
-        <CustomCheckbox>Task 1 </CustomCheckbox>
+        {/* <CustomCheckbox>Task 1 </CustomCheckbox>
         <CustomCheckbox>mucho texto mucho texto mnucho texto </CustomCheckbox>
-        <CustomCheckbox>mmucho texto mucho texto mucho texto ucho texto mucho texto mucho texto </CustomCheckbox>
+        <CustomCheckbox>mmucho texto mucho texto mucho texto ucho texto mucho texto mucho texto </CustomCheckbox> */}
+
+        {tasks.map((task, index) => (
+          <CustomCheckbox key={index}>{task.task}</CustomCheckbox>
+        ))}
       </Box>
 
       <AddTaskButton/>
